5e07aa20e60dd9c17e1cb385ba15d81f31b353fe
Mityasaurus <dmitriybovdun5@gmail.com>
2025-02-22 19:19:35 +0200

    Add project files.

---
+++ MoneyMate_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoneyMate_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
